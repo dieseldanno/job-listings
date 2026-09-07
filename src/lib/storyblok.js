9afb72a3bc42e04c14f9d6319dd41c686e9861e7
dieseldanno <danno_95@hotmail.com>
2026-09-07 10:58:01 +0200

    Initial commit: Storyblok quickstart

---
+++ src/lib/storyblok.js
@@ -0,0 +1,18 @@
+import Page from '@/components/Page';
+import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
+
+export const getStoryblokApi = storyblokInit({
+	accessToken: process.env.STORYBLOK_DELIVERY_API_TOKEN,
+	use: [apiPlugin],
+	components: {
+		page: Page,
+	},
+	apiOptions: {
+		/** Set the correct region for your space. Learn more: https://www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
+		region: process.env.STORYBLOK_REGION || 'eu',
+		/** The following code is only required when creating a Storyblok space directly via the Blueprints feature. */
+		endpoint: process.env.STORYBLOK_API_BASE_URL
+			? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2`
+			: undefined,
+	},
+});
